a59438a22e907f5a64e5dcc7114a981a580d4d26
Watanabe, Shunsuke | Syun | CPD <shunsuke.a.watanabe@rakuten.com>
2022-02-06 16:13:38 +0900

    developping

--- onsenswap_factory/lib.rs
+++ onsenswap_factory/lib.rs
@@ -5,43 +5,52 @@ use ink_lang as ink;
 #[ink::contract]
 mod onsenswap_factory {
 
+    const MINIMUM_LIQUIDITY: u128 = 1000; // 10 ** 3 
+
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
+    #[derive(Default)]
     #[ink(storage)]
     pub struct OnsenswapFactory {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        factory: AccountId,
+        token0: AccountId,
+        token1: AccountId,
+
+        reserve0: u128,
+        reserve1: u128,
+        blockTimestampLast: u32,
+
+        price0CumulativeLast: u128,
+        price1CumulativeLast: u128,
+        kLast: u128,
     }
 
     impl OnsenswapFactory {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(
+            factory: AccountId,
+            token0: AccountId,
+            token1: AccountId,
+        ) -> Self {
+            // message sender
+            let caller = Self::env().caller();
+            let mut onsenswapFactory: OnsenswapFactory = Default::default();
+            onsenswapFactory.factory = caller;
+            onsenswapFactory.token0 = token0;
+            onsenswapFactory.token1 = token1;
+
+            onsenswapFactory
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn get_reserves(&self) -> (u128, u128, u32) {
+            (self.reserve0, self.reserve1, self.blockTimestampLast)
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
+        pub fn mint()
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
@@ -54,13 +63,6 @@ mod onsenswap_factory {
 
         /// Imports `ink_lang` so we can use `#[ink::test]`.
         use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let onsenswap_factory = OnsenswapFactory::default();
-            assert_eq!(onsenswap_factory.get(), false);
-        }
 
         /// We test a simple use case of our contract.
         #[ink::test]
